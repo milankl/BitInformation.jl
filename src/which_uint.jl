f371e21ad0beee627a6ad53d36e1e44d0ed6a5a3
Milan <milankloewer@gmx.de>
2022-02-28 19:43:36 +0000

    bitpattern entropy overhauled, uinttype for (u)ints

--- src/which_uint.jl
+++ src/which_uint.jl
@@ -3,7 +3,19 @@ function whichUInt(n::Integer)
     n == 16 && return UInt16
     n == 32 && return UInt32
     n == 64 && return UInt64
-    throw(error("Only n=8,16,32,64 supported."))
+    throw(error("Only n=8,16,32,64 bits supported."))
 end
 
 whichUInt(::Type{T}) where T = whichUInt(sizeof(T)*8)
+
+# define the uints for various formats
+Base.uinttype(::Type{UInt8}) = UInt8
+Base.uinttype(::Type{UInt16}) = UInt16
+Base.uinttype(::Type{UInt32}) = UInt32
+Base.uinttype(::Type{UInt64}) = UInt64
+
+Base.uinttype(::Type{Int8}) = UInt8
+Base.uinttype(::Type{Int16}) = UInt16
+Base.uinttype(::Type{Int32}) = UInt32
+Base.uinttype(::Type{Int64}) = UInt64
+
