24ce428257bed8763a9e8769fae903baf71e9e9f
Milan <milankloewer@gmx.de>
2022-03-22 13:13:56 +0000

    permutations.jl included

--- src/BitInformation.jl
+++ src/BitInformation.jl
@@ -1,12 +1,15 @@
 module BitInformation
 
+    # ROUNDING
     export shave, set_one, groom, halfshave, 
         shave!, set_one!, groom!, halfshave!, round!
 
+    # TRANSFORMATIONS
     export bittranspose, bitbacktranspose,
         xor_delta, unxor_delta, xor_delta!, unxor_delta!,
         signed_exponent, biased_exponent
     
+    # INFORMATION
     export bitinformation, mutual_information, redundancy, bitpattern_entropy,
         bitcount, bitcount_entropy, bitpaircount, bit_condprobability,
         bit_condentropy
@@ -14,14 +17,24 @@ module BitInformation
     import StatsBase: entropy
     import Distributions: quantile, Normal
 
+    # Base method extensions
     include("uint_types.jl")
+
+    # print and conversions
     include("bitstring.jl")
     include("bitarray.jl")
+
+    # transformations
     include("bittranspose.jl")
-    include("round_nearest.jl")
-    include("shave_set_groom.jl")
     include("xor_delta.jl")
     include("signed_exponent.jl")
+
+    # rounding
+    include("round_nearest.jl")
+    include("shave_set_groom.jl")
+    
+    # information
+    include("permutations.jl")
     include("bit_count.jl")
     include("mutual_information.jl")
     include("bitpattern_entropy.jl")
